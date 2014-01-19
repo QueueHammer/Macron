a2bbda6c807ef8631af148b1a81a338b3fad2fd7
Christopher Smith <chrismsmith@gmail.com>
2014-01-19 14:11:53 -0500

    Added nested properties to min, max
    Added the nested lookup to min and max. Looked a little more into how to
    call the original function of _ when the call signatures are the
    originals.

--- macron.js
+++ macron.js
@@ -6,33 +6,54 @@ may change to underscore.nested
 (function () {
   // Establish the root object, `window` in the browser, or `global` on the server.
   var root = this;
-
-  // Save the previous value of the `_` variable.
-  var _base = root._;
-  
+    
   //There are many untility libraries, but this one we depend on.
   //With out it we are nothing, with out us... well you get the picture
-  if(root._ === undefined || root._.mixin == undefined) return;
+  if (root._ === undefined || root._.mixin == undefined) return;
+
+  // Save the previous value of the `_` variable.
+  var _clone = _.clone(_);
   
-  this._.macronVersion  = '0.0.1';
+  _.macron  = '0.0.1';
 
   //Regex to detect when we want to run nested functions
   var nestedPropRegex = /([^\.]+\.)[^\.]+/;
   //Get return a function to get the nested value from an object based on
   //the property chain provided.
-  var ferrit = function(propChain) {
+  var ferrit = function (propChain) {
+    var splitProps = propChain.split('.');
     return function (obj) {
-      return _.reduce(propChain.split('.'), function (m, d, i) {
+      return _.reduce(splitProps, function (m, d, i) {
         return d in m ? m[d] : undefined;
       }, obj);
     };
   };
   
   var nestedExtentionFunctions = {
+    //## pluck
     pluck: function (list, propChain) {
       var f = ferrit(propChain);
       return _.map(list, function (d) { return f(d); });
     },
+    //## max
+    max: function (list, prop) {
+      var f = function (i) { return i; };
+      if (prop !== undefined) f = ferrit(prop);
+      return _.reduce(list, function (m, v) {
+        var n = f(v);
+        return n > m ? n : m;
+      }, -Infinity);
+    },
+    //## min
+    min: function (list, prop) {
+      var f = function (i) { return i; };
+      if (prop !== undefined) f = ferrit(prop);
+      return _.reduce(list, function (m, v) {
+        var n = f(v);
+        return n < m ? n : m;
+      }, Infinity);
+    },
+    //## sortBy
     sortBy: function (list) {
       //Return a function to sort based on the function passed
       var sortWith = function(f){
@@ -42,8 +63,6 @@ may change to underscore.nested
           return 0;
         };
       };
-      
-      
       
       //Create a list of functions to sort by
       //based on a list of nested properties and/or functions
@@ -60,9 +79,15 @@ may change to underscore.nested
           return m !== 0 ? m : d(a, b);
         }, 0);
       }); 
-    }
+    },
+    //groupBy: function () {
+    //},
+    //indexBy: function () {
+    //},
+    //countBy: function () {
+    //}
   };
   
-  _.mixin(nestedExtentionFunctions);
+  root._.mixin(nestedExtentionFunctions);
   
 }).call(this);
